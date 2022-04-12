8dab813ca2256d4499e1c1c918faaa7baf62d63b
seema77 <76130253+seema77@users.noreply.github.com>
2022-04-13 03:41:45 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,29 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120, 100);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+ 
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+
+  display() {
+   this.setElementsPosition();
+  
+  }
+}
